54ee40a807639a96d5c4d1f494b61ec028e61f3e
keomony khun <keomony@gmail.com>
2016-12-31 11:49:36 +0000

    isolate dependencies: by moving dependencies to initializer if they can't be changed outside the class

--- gear.rb
+++ gear.rb
@@ -1,16 +1,21 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, wheel)
+  attr_reader :chainring, :cog, :rim, :tire, :wheel
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
+    @wheel = Wheel.new(rim, tire)
   end
 
-  def gear_inches
-    ratio * wheel.diameter
+  def diameter
+    wheel.diameter
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    ratio * diameter
+  end
+
 end
